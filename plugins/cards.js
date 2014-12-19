a4eec5a61bb5f2dca70905e83bb816d5d4576280
Anand Thakker <vestibule@anandthakker.net>
2014-12-19 15:41:38 +0100

    rss feed

--- plugins/cards.js
+++ plugins/cards.js
@@ -5,31 +5,31 @@ module.exports = function(env, callback) {
   _ = env.locals._;
   
   env.helpers.getCardData = getCardData;
-  function getCardData(contents) {
+  function getCardData(contents, path) {
     var ret = [];
-    
+    path = path ? path : []
     for(x in contents)
     {
       var item = contents[x];
       if(item.url) {
         ret.push({
+          key: path.concat(x),
           url: item.url,
           metadata: item.metadata,
           content: item.markdown
         });
       }
       else {
-        ret.push.apply(ret, getCardData(item));
+        ret.push.apply(ret, getCardData(item, path.concat(x)));
       }
     }
-    // articles = _.values(contents[options.articles])
-    // articles = articles.filter (art)->
-    // art.intro?
-    // .filter (art) ->
-    // (not tags?) or _.intersection(tags, art.metadata.tags ? []).length > 0
-    // articles.sort (a, b) -> b.date - a.date
-    // return articles
     return ret;
+  }
+  
+  env.helpers.getPropertyPath = function(content, path) {
+    var prop, obj = content;
+    while(prop = path.shift()) obj = obj[prop];
+    return obj;
   }
   
   return callback();
