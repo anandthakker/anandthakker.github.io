ab99e953fd7aeab1b4ac6cc82d4a3b34861b64de
Anand Thakker <vestibule@anandthakker.net>
2015-11-26 17:02:46 -0500

    Switch to circle-ci

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ cd build
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "<you>@<your-email>"
+git config user.name "CI Robot"
+git config user.email "robot@anand.codes"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
